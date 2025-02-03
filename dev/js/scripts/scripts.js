1681ae4b2761a6d8aed556c264b2f99255d5d458
a-macro <73707239+a-macro@users.noreply.github.com>
2025-02-03 13:34:13 +0300

    venobox, validation

--- dev/js/scripts/scripts.js
+++ dev/js/scripts/scripts.js
@@ -29,5 +29,58 @@ window.addEventListener('DOMContentLoaded', () => {
             document.documentElement.style.setProperty('--w', winW + 'px');
         }, 100);
     });
+
+    const vboxOptions = {
+        overlayColor: 'rgba(46, 46, 46, 0.4)',
+        bgcolor: null,
+        overlayClose: true,
+        onContentLoaded: (newcontent) => {
+            initPopup();
+            setSubmitVal();
+        },
+    };
+    let vBox;
+    if(VenoBox) {
+        vBox = new VenoBox(vboxOptions);
+        window.vBox = vBox;
+    }
+      
+    document.addEventListener('click', (e) => {
+        const dataAction = 'data-action';
+        const eTarget = e.target.closest(`[${dataAction}]`);
+        const activeClass = '_active';
+      
+        const vBoxLink = e.target.closest('[data-venobox]');
+        if (vBoxLink) {
+            const href = vBoxLink.getAttribute('href');
+        
+            if (href) {
+                e.preventDefault();
+                if (!vBoxLink.settings) {
+                    vBoxLink.settings = vBox.settings;
+                }
+                vBox.close();
+                setTimeout(() => {
+                    vBox.open(vBoxLink);
+                }, 500);
+            }
+        }
+      
+        if (eTarget) {
+            const actions = eTarget.getAttribute('data-action').split(' ');
+        
+            for (let action of actions) {
+                switch (action) {
+                    case 'vbox-close': {
+                        vBox.close();
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+    });
 });
 
